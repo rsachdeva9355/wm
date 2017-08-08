18bc444bf149de8eb509d9fc7650a0a6235ada65
Tanvi <tanvikant31@gmail.com>
2017-08-09 02:34:53 +0530

    Added ProductDO

---
+++ warehouse-manager/src/main/java/com/rtv/store/ProductDO.java
@@ -0,0 +1,27 @@
+package com.rtv.store;
+
+import org.mongodb.morphia.annotations.Entity;
+import org.mongodb.morphia.annotations.Id;
+import org.mongodb.morphia.annotations.Property;
+
+/**
+ * Created by Tanvi on 09/08/17.
+ */
+
+@Entity(value = "product", noClassnameStored = true)
+public class ProductDO {
+    @Id
+    private String id;
+
+    @Property
+    private String productID;
+
+    @Property
+    private String companyProductName;
+
+    @Property
+    private String companyProduct;
+
+    @Property
+    private String productName;
+}
