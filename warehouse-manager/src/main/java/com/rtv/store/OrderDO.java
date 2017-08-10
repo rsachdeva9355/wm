87c52fd1e854125fc02b0265d86287a4431ddde4
Tanvi <tanvikant31@gmail.com>
2017-08-11 01:23:41 +0530

    Added properties to OrderDO

--- warehouse-manager/src/main/java/com/rtv/store/OrderDO.java
+++ warehouse-manager/src/main/java/com/rtv/store/OrderDO.java
@@ -15,6 +15,33 @@ public class OrderDO {
     @Property
     private String orderType;
 
+    @Property
+    private String userID;
+
+    @Property
+    private Long date;
+
+    @Property
+    private Long thirdPartyID;
+
+    @Property
+    private String productID;
+
+    @Property
+    private String batchID;
+
+    @Property
+    private Float costPrice; //of individual product
+
+    @Property
+    private Float gst;
+
+    @Property
+    private Float totalCost;
+
+    @Property
+    private Integer quantity;
+
     private OrderDO() {}
 
     public String getId() {
@@ -33,4 +60,79 @@ public class OrderDO {
         this.orderType = orderType.name();
     }
 
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public Long getDate() {
+        return date;
+    }
+
+    public void setDate(Long date) {
+        this.date = date;
+    }
+
+    public Long getThirdPartyID() {
+        return thirdPartyID;
+    }
+
+    public void setThirdPartyID(Long thirdPartyID) {
+        this.thirdPartyID = thirdPartyID;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getBatchID() {
+        return batchID;
+    }
+
+    public void setBatchID(String batchID) {
+        this.batchID = batchID;
+    }
+
+    public Float getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Float costPrice) {
+        this.costPrice = costPrice;
+    }
+
+    public Float getGst() {
+        return gst;
+    }
+
+    public void setGst(Float gst) {
+        this.gst = gst;
+    }
+
+    public Float getTotalCost() {
+        return totalCost;
+    }
+
+    public void setTotalCost(Float totalCost) {
+        this.totalCost = totalCost;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
