d00e074cf0eb95ffa7cf20d178bbd5515d193b94
Tanvi <tanvikant31@gmail.com>
2017-08-11 00:34:07 +0530

    Added doubts for creating apis

--- warehouse-manager/src/main/java/com/rtv/OrderResource.java
+++ warehouse-manager/src/main/java/com/rtv/OrderResource.java
@@ -1,12 +1,20 @@
 package com.rtv;
 
+import com.codahale.metrics.annotation.Timed;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rtv.models.Order;
+import com.rtv.store.OrderDO;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.mongodb.morphia.Datastore;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -30,5 +38,23 @@ public class OrderResource {
     {
         this.store = store;
         this.objectMapper = objectMapper;
+    }
+
+    @POST
+    @Timed
+    @ApiOperation(value = "Create a new order")
+    @Produces(MediaType.APPLICATION_JSON)
+    public @Valid
+    Order create(
+            @Valid
+            @NotNull
+            @ApiParam("order to be created")
+            Order order
+                )
+    {
+        OrderDO orderDO;
+        //TODO - code for validating this order will go where?
+        
+        return //TODO - kya return karna hai kaise?
     }
 }
