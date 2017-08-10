95f4e2491c9a0b32d038f5df6b24538ccf83bfc0
Tanvi <tanvikant31@gmail.com>
2017-08-11 01:28:20 +0530

    Removed todos

--- warehouse-manager/src/main/java/com/rtv/models/Order.java
+++ warehouse-manager/src/main/java/com/rtv/models/Order.java
@@ -5,14 +5,4 @@ package com.rtv.models;
  * Created by Tanvi on 11/08/17.
  */
 public class Order {
-    //TODO - What do I keep here? I just deleted all the code I had earlier written
-    //which kept all the DOs whose data would be returned.
-
-    //TODO - Do I need to keep each data member corresponding to the json keys or would setters and getters suffice?
-
-    //TODO - How do I map the data here to the DOs? Do I create two different kind of constructors?
-    //One with DO objects as params and the other with the data members that go in them
-
-    //TODO - Orders ka DAO kaha hoga? Jo GET call k liye dhund k laega?
-
 }
