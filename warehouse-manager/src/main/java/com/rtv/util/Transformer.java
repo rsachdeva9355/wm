900fa5d89fb17b9a0eeff8f5a9561bf1f8c2b29b
Rachit Sachdeva <rsachdev@adobe.com>
2017-08-22 02:44:29 +0530

    Added id to bill

--- warehouse-manager/src/main/java/com/rtv/util/Transformer.java
+++ warehouse-manager/src/main/java/com/rtv/util/Transformer.java
@@ -1,5 +1,8 @@
 package com.rtv.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.rtv.api.auth.Batch;
 import com.rtv.api.auth.Bill;
 import com.rtv.api.auth.Order;
@@ -17,9 +20,6 @@ import com.rtv.store.ThirdPartyDAO;
 import com.rtv.store.ThirdPartyDO;
 import com.rtv.store.UserDAO;
 import com.rtv.store.UserDO;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Transformer {
 
@@ -154,6 +154,7 @@ public class Transformer {
             return null;
         }
         Bill bill = new Bill();
+        bill.setId(billDO.getId());
         bill.setBillType(billDO.getBillType());
         bill.setBillNumber(billDO.getBillNumber());
         bill.setThirdParty(ThirdPartyDAO.getThirdPartyByID(billDO.getThirdPartyID()));
